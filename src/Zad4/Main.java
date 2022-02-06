1037362cb40fe3749aba0bc71406d368ef12a98e
Anton Bezhik <theslimbo@gmail.com>
2022-02-06 20:08:22 +0300

    bla bla

--- src/Zad4/Main.java
+++ src/Zad4/Main.java
@@ -1,9 +1,14 @@
 package Zad4;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
         int num = 14511;
+        Scanner field = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        num = field.nextInt();
         GraphicNumber test = new GraphicNumber(num);
         test.PrintNumbers();
     }
